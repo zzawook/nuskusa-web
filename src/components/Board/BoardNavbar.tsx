241a64a0de74c72c7bd4a1d7b0e68251fb7e9609
Yong Gi <rygrobin@gmail.com>
2021-10-18 20:34:55 +0800

    fix

--- src/components/Board/BoardNavbar.tsx
+++ src/components/Board/BoardNavbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { dbService } from '../../utils/FirebaseFunctions'
+import { dbService } from '../../utils/firebaseFunctions'
 import { DisplaySmall, Headline } from '../../utils/ThemeText'
 
 type BoardProps = {
