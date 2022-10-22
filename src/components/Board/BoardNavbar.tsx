9202d6ed39e56d8f615c004b33d1904d989faac2
kjaehyeok21 <kjaehyeok21@gmail.com>
2022-10-22 17:59:02 +0800

    Fixed BoardNavbar not updating

--- src/components/Board/BoardNavbar.tsx
+++ src/components/Board/BoardNavbar.tsx
@@ -4,6 +4,27 @@ import styled from 'styled-components'
 import { User } from '../../types/User'
 import { Headline } from '../../utils/ThemeText'
 
+const LinkContainer = styled.div`
+    display: inline-block;
+    opacity: 0.5;
+    margin: 5px 10px;
+    white-space: nowrap;
+    :hover {
+        opacity: 1;
+    }
+`
+const CurrentContainer = styled.div`
+    display: inline-block;
+    opacity: 1;
+    margin: 5px 10px;
+    white-space: nowrap;
+`
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 70vw;
+`
+
 type BoardNavbarProps = {
     currentRoute: string,
     userData: User,
@@ -11,6 +32,7 @@ type BoardNavbarProps = {
 
 type BoardNavbarState = {
     componentArray: any[],
+    boardData: any[],
 }
 
 // Navigation bar for choosing individual boards
@@ -20,34 +42,35 @@ class BoardNavbar extends React.Component<BoardNavbarProps, BoardNavbarState> {
         super(props)
         this.state = {
             componentArray: [],
+            boardData: []
         }
     }
 
     fetchBoards = async () => {
-        const LinkContainer = styled.div`
-            display: inline-block;
-            opacity: 0.5;
-            margin: 5px 10px;
-            white-space: nowrap;
-            :hover {
-                opacity: 1;
-            }
-        `
-        const CurrentContainer = styled.div`
-            display: inline-block;
-            opacity: 1;
-            margin: 5px 10px;
-            white-space: nowrap;
-        `
-
         const componentArray: any = []
+        const boardData: any[] = []
         const url = process.env.REACT_APP_HOST + '/api/board/getBoards'
         const response = await fetch(url)
         if (response.status == 200) {
             const boards = await response.json()
             for (let i = 0; i < boards.length; i++) {
-                if (boards[i].boardId !== this.props.currentRoute) {
-                    componentArray.push(
+                boardData.push(boards[i])
+            }
+            this.setState({
+                boardData: componentArray
+            })
+        }
+    }
+
+    generateComponent = () => {
+        if (this.state.boardData.length === 0) {
+            return
+        }
+        const boards = this.state.boardData
+        const componentArray: any[] = []
+        for (let i = 0; i < boards.length; i++) {
+            if (boards[i].boardId !== this.props.currentRoute) {
+                componentArray.push(
                     <LinkContainer key={i}>
                         <Link to={{ pathname: `/boards/${boards[i].boardId}` }}
                             style={{ textDecoration: 'none' }}
@@ -57,25 +80,24 @@ class BoardNavbar extends React.Component<BoardNavbarProps, BoardNavbarState> {
                             </Headline>
                         </Link>
                     </LinkContainer >)
-                }
-                else {
-                    componentArray.push(
-                        <CurrentContainer key={i}>
-                            <Link to={{ pathname: `/boards/${boards[i].boardId}` }}
-                                style={{ textDecoration: 'none' }}
-                            >
-                                <Headline color='white'>
-                                    {boards[i].title}
-                                </Headline>
-                            </Link>
-                        </CurrentContainer>
-                    )
-                }
             }
-            this.setState({
-                componentArray: componentArray
-            })
+            else {
+                componentArray.push(
+                    <CurrentContainer key={i}>
+                        <Link to={{ pathname: `/boards/${boards[i].boardId}` }}
+                            style={{ textDecoration: 'none' }}
+                        >
+                            <Headline color='white'>
+                                {boards[i].title}
+                            </Headline>
+                        </Link>
+                    </CurrentContainer>
+                )
+            }
         }
+        this.setState({
+            componentArray: componentArray,
+        })
     }
 
     componentDidMount = () => {
@@ -85,16 +107,15 @@ class BoardNavbar extends React.Component<BoardNavbarProps, BoardNavbarState> {
         }
     }
 
+    componentDidUpdate = () => {
+        this.generateComponent()
+    }
+
     componentWillUnmount = () => {
         this._isMounted = false;
     }
 
     render = () => {
-        const Wrapper = styled.div`
-            display: flex;
-            flex-direction: row;
-            width: 70vw;
-        `
         return (
             <Wrapper>
                 {this.state.componentArray}
